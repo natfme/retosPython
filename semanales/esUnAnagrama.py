7205204faaa61a9c336a857955227144faa0ae6f
NatFMe <ing.nataliafrancomedina@gmail.com>
2023-01-10 14:57:32 -0500

    add-2reto

---
+++ semanales/esUnAnagrama.py
@@ -0,0 +1,17 @@
+""" Reto #1
+¿ES UN ANAGRAMA?
+Fecha publicación enunciado: 03/01/22
+Fecha publicación resolución: 10/01/22
+Dificultad: MEDIA
+
+Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+NO hace falta comprobar que ambas palabras existan.
+Dos palabras exactamente iguales no son anagrama. """
+
+print("Escriba la primera palabra")
+word1 = input()
+word1 = word1.lower()
+word2 = input()
+word2 = word2.lower()
+print(word1+" "+word2)
--- semanales/fizzbuzz.py
+++ semanales/fizzbuzz.py
@@ -1,9 +1,15 @@
-""" Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+"""Reto #0
+EL FAMOSO "FIZZ BUZZ"
+Fecha publicación enunciado: 27/12/21
+Fecha publicación resolución: 03/01/22
+Dificultad: FÁCIL
+
+Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
  * - Múltiplos de 3 por la palabra "fizz".
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz" """
 
-for i in range(1, 101):
+for i in range(1, 101):  #Range incluye el 1, incrementa de 1 en uno y va hasta 100, por eso se pone hasta 101, para que me incluya el 100
     divisiblePorTres = i % 3 == 0
     divisiblePorCinco = i % 5 == 0
 
